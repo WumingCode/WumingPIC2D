b9113b3dc5d2f478468da16ec25bf5e44f72447c
Takanobu Amano <amano@eps.s.u-tokyo.ac.jp>
2021-06-15 00:50:47 +0900

    added Fujitsu compiler configuration

---
+++ compiler-fujitsu.mk
@@ -0,0 +1,11 @@
+# -*- Makefile -*-
+
+# HDF5
+HDF5DIR = $(shell spack location -i hdf5 %fj /7cxah5)
+
+# compilers and arguments
+AR      = ar
+CC      = mpifccpx -Kfast,openmp
+FC      = mpifrtpx -Kfast,openmp
+FCFLAGS = -Nalloc_assign -Cpp -I$(BASEDIR)/include -I$(HDF5DIR)/include
+LDFLAGS = -L$(BASEDIR)/lib -L$(HDF5DIR)/lib
